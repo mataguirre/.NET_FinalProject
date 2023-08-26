868b7b97de274631da700c46d0115576cc65a720
mataguirre <mataguirre.dev@gmail.com>
2023-08-26 00:46:09 -0300

    Updating proyect

---
+++ Ejercicio/EditarEmpleadoForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Ejercicio
+{
+    partial class EditarEmpleadoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkCasado = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textDocument = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textLastName = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textSalario = new System.Windows.Forms.TextBox();
+            this.textEdad = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "DNI";
+            // 
+            // checkCasado
+            // 
+            this.checkCasado.AutoSize = true;
+            this.checkCasado.Location = new System.Drawing.Point(118, 241);
+            this.checkCasado.Name = "checkCasado";
+            this.checkCasado.Size = new System.Drawing.Size(37, 17);
+            this.checkCasado.TabIndex = 3;
+            this.checkCasado.Text = "Sí";
+            this.checkCasado.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Casado";
+            // 
+            // textDocument
+            // 
+            this.textDocument.Location = new System.Drawing.Point(118, 160);
+            this.textDocument.Name = "textDocument";
+            this.textDocument.Size = new System.Drawing.Size(136, 20);
+            this.textDocument.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Salario";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(179, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textLastName
+            // 
+            this.textLastName.Location = new System.Drawing.Point(118, 124);
+            this.textLastName.Name = "textLastName";
+            this.textLastName.Size = new System.Drawing.Size(136, 20);
+            this.textLastName.TabIndex = 9;
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(118, 89);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(136, 20);
+            this.textName.TabIndex = 10;
+            // 
+            // textSalario
+            // 
+            this.textSalario.Location = new System.Drawing.Point(118, 278);
+            this.textSalario.Name = "textSalario";
+            this.textSalario.Size = new System.Drawing.Size(136, 20);
+            this.textSalario.TabIndex = 11;
+            // 
+            // textEdad
+            // 
+            this.textEdad.Location = new System.Drawing.Point(118, 202);
+            this.textEdad.Name = "textEdad";
+            this.textEdad.Size = new System.Drawing.Size(136, 20);
+            this.textEdad.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(64, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Edad";
+            // 
+            // EditarEmpleadoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 450);
+            this.Controls.Add(this.textEdad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textSalario);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.textLastName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textDocument);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkCasado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "EditarEmpleadoForm";
+            this.Text = "Modificar Empleado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkCasado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textDocument;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textLastName;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textSalario;
+        private System.Windows.Forms.TextBox textEdad;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ Ejercicio/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    internal class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string DNI { get; set; }
+        public int Edad { get; set; }
+        public bool Casado { get; set; }
+        public decimal Salario { get; set; }
+    }
+}
